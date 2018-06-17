9bfb7ef4fcb6ae1e4e59b3725776fdedc3025b27
lucasoli <lksfx@hotmail.com>
2018-06-17 14:20:10 -0300

    And god see that's good.

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class CardScript : MonoBehaviour {
+
+    Animator animator;
+    static string[] suitList = new string[] { "Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
+        "Uranus", "Neptune", "Pluto", "Moon" };
+
+    // Isto ocorre antes do Start
+    void Awake () {
+        animator = GetComponent<Animator>();
+    }
+
+    // Isto ocorre após o Awake
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void flipCard() {
+        // inverte posição da carta
+        flipCard(!animator.GetBool("Flipped")); 
+    }
+
+    // Vira a carta para cima quando true, ou vice-versa.
+    public void flipCard(bool stat) {
+        animator.SetBool("Flipped", stat);
+        StartCoroutine(ShowInfoWithDelay(stat, stat ? 0.45f : 0.1f));
+    }
+
+    IEnumerator ShowInfoWithDelay(bool show, float delay) {
+        yield return new WaitForSeconds(delay);
+        transform.Find("Info").gameObject.SetActive(show);
+    }
+
+    // definir a carta pelo nome
+    public void setCard(string name) { }
+
+    // definir a carta pelo número
+    public void setCard(int value) {
+        if (value >= suitList.Length || value < 0) return; // evita valor inexistente
+        //if (animator == null) animator = GetComponent<Animator>();
+        string thisCardName = suitList[value];
+        RuntimeAnimatorController controller = Resources.Load<RuntimeAnimatorController>("Animator/" + thisCardName);
+        animator.runtimeAnimatorController = controller;
+        Text numberLabel = transform.Find("Info/Number").GetComponent<Text>();
+        Text nameLabel = transform.Find("Info/Name").GetComponent<Text>();
+        numberLabel.text = value.ToString();
+        nameLabel.text = thisCardName;
+    }
+}
---
+++ Assets/Scripts/DeckBaseScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeckBaseScript : MonoBehaviour {
+
+    CardScript[] deck;
+    public bool debug = false;
+
+	// Use this for initialization
+	void Start () {
+        // todas as cartas ficam neste array
+        deck = GetComponentsInChildren<CardScript>();
+        int[] Nipes = obtainNipes(4, 0, 10);
+        int[] NipesDuplicated = obtainDuplicatedExceptOne(Nipes);
+        List<int> cardList = new List<int>(NipesDuplicated);
+        foreach (CardScript card in deck) {
+            // randomiza posição das cartas pela última vez
+            int randomVal = UnityEngine.Random.Range(0, cardList.Count - 1);
+            int chooseCard = cardList[randomVal];
+            card.setCard(chooseCard);
+            cardList.RemoveAt(randomVal);
+            // ---------
+            //Debug.Log("choose is " + ": " + chooseCard);
+        }
+        StartCoroutine(ShowDeckMode(0, 2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator ShowDeckMode(int mode, float after) {
+        yield return new WaitForSeconds(after);
+        StartCoroutine(ShowFullHouse(3));
+        Debug.Log("Show Deck Start");
+    }
+
+    IEnumerator ShowFullHouse(float duration) {
+        foreach (CardScript card in deck) {
+            card.flipCard(true);
+        }
+        yield return new WaitForSeconds(duration);
+        //foreach (CardScript card in deck) {
+        //    card.flipCard(false);
+        //}
+        Debug.Log("Show Deck End");
+    }
+
+    // retorna 3 inteiros não repetidos entre determinados valores
+    int[] obtainNipes(int size, int min, int max) {
+        if (min > max) return new int[0]; // mínimo não pode ser maior que o máximo
+        max = Mathf.Max(size, max); // tamanho máximo não pode ser menor que o mínimo de valores retornados
+        int[] values = new int[size];
+        // evita que o array inicie com valores zero, o que inviabilizaria a seleção desse valor
+        for (int i = 0; i < values.Length; i++) { values[i] = -1; }
+        // se o tamanho for igual ao número máximo, retorna sequencial
+        if (size == max) {
+            for (int i = 0; i < values.Length; i++) {
+                values[i] = i;
+            }
+        }
+        // se o tamanho for menor que o número máximo, sorteia aleatoriamente
+        else {
+            int currentVal;
+            List<int> valuesList = new List<int>(values);
+            for (int i = 0; i < valuesList.Count; i++) {
+                currentVal = UnityEngine.Random.Range(min, max);
+                // Este ciclo ocorre enquanto o valor aleatório já estiver na lista, visto que só pode ocorrer um de cada. 
+                while (valuesList.Contains(currentVal)) {
+                    currentVal++; // se o valor escolhido aleatoriamente já estiver contido na lista, passa para o próximo
+                    if (currentVal > max) currentVal = min;
+                }
+                valuesList[i] = currentVal;
+                if (debug) Debug.Log("Nipe " + i + ": " + currentVal);
+            }
+            values = valuesList.ToArray();
+        }
+        return shuffle(values); // retornar valores embaralhados
+    }
+
+    int[] obtainDuplicatedExceptOne(int[] entry) {
+        int[] values = new int[(entry.Length*2)-1];
+        int wildcard = entry[UnityEngine.Random.Range(0, entry.Length-1)]; //escolhe um dos números da lista para ser o curinga
+        for (int i = 0, z = 0; i < entry.Length; i++) { // duplica os valores que não forem o curinga
+            values[z++] = entry[i];
+            if (entry[i] == wildcard) continue;
+            values[z++] = entry[i];
+        }
+        if (debug) Debug.Log("NipeDuplicated " + String.Join("", new List<int>(values).ConvertAll(i => i.ToString()).ToArray()));
+        return values;
+    }
+
+    int[] shuffle(int[] entry) {
+        List<int> list = new List<int>(entry);
+        int[] values = new int[entry.Length];
+        int choose;
+        for (int i = 0; i < entry.Length; i++) {
+            choose = UnityEngine.Random.Range(0, list.Count);
+            values[i] = list[choose];
+            list.RemoveAt(choose);
+        }
+        return values;
+    }
+}
